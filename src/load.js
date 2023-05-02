0b455287e7ee0fe9cc0d14c9c8ac8648ade988a7
LozzyChops <lephilim@gmail.com>
2023-05-02 17:28:03 -0500

    Change modules to remove div before adding content

--- src/load.js
+++ src/load.js
@@ -1,20 +1,28 @@
-import pic from './hungry_cat.jpg';
+import pic from "./hungry_cat.jpg";
 
 function initLoad() {
-    const content = document.getElementById('content');
+  const content = document.getElementById("content");
 
-    const myImage = new Image();
-    myImage.src = pic;
-    myImage.setAttribute('alt', 'A hungry cat');
+  const remove = document.querySelector('div:last-child');
+  remove.parentElement.removeChild(remove);
 
-    const text = document.createElement('p');
-    const quote = document.createElement('q');
-    quote.textContent = "Fur real, it's the cat's meow!";
-    
-    content.appendChild(myImage);
-    text.appendChild(quote);
-    text.append(' - Caterday Magazine');
-    content.appendChild(text);
-};
+  const main = document.createElement("div");
+  main.setAttribute("id", "main");
+  main.className = "flex-container";
+
+  const myImage = new Image();
+  myImage.src = pic;
+  myImage.setAttribute("alt", "A hungry cat");
+
+  const text = document.createElement("p");
+  const quote = document.createElement("q");
+  quote.textContent = "Fur real, it's the cat's meow!";
+
+  main.appendChild(myImage);
+  text.appendChild(quote);
+  text.append(" - Caterday Magazine");
+  main.appendChild(text);
+  content.appendChild(main);
+}
 
 export default initLoad;
--- src/load_contact.js
+++ src/load_contact.js
@@ -1,27 +1,30 @@
 function initContact() {
   const content = document.getElementById("content");
+ 
+  const remove = document.querySelector('div:last-child');
+  remove.parentElement.removeChild(remove);
 
-  const contact = document.createElement('div');
+  const contact = document.createElement("div");
   contact.setAttribute("id", "contact");
-  contact.className = 'flex-container';
+  contact.className = "flex-container";
 
-  const contactHeader = document.createElement('h2');
-  contactHeader.textContent = 'Contact Us';
+  const contactHeader = document.createElement("h2");
+  contactHeader.textContent = "Contact Us";
 
-  const phone = document.createElement('p');
-  phone.textContent = 'Phone: (987) 654-3210';
+  const phone = document.createElement("p");
+  phone.textContent = "Phone: (987) 654-3210";
 
-  const email = document.createElement('p');
-  email.textContent = 'Email: contact@puurfect.com';
+  const email = document.createElement("p");
+  email.textContent = "Email: contact@puurfect.com";
 
-  const address = document.createElement('p');
-  address.textContent = 'Address:  987 Empty Box Street, Catton, 98765';
+  const address = document.createElement("p");
+  address.textContent = "Address:  987 Empty Box Street, Catton, 98765";
 
   contact.appendChild(contactHeader);
   contact.appendChild(phone);
   contact.appendChild(email);
   contact.appendChild(address);
   content.appendChild(contact);
-};
+}
 
 export default initContact;
